bbe381a52c369a6f4242eac5048a6ebc0267951d
Comrad112 <comrad112@gmail.com>
2021-07-06 01:25:52 -0400

    up

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+    },
+
+    discord: {
+        token: 'ODYxNzY5MDc0MTY1Mjg0ODc0.YOOm_g.L_6pGFLO_B2xWHTpctt6IdBUSnI',
+        prefix: 'a!',
+        activity: 'Music',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
